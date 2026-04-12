eda4a9f15f525186e796caa2906074ab06a48387
darrenkearney171-dot <darrenkearney171@hotmail.co.uk>
2026-04-12 13:15:02 +0100

    Auto-push: server/fitted.ts

---
+++ server/fitted.ts
@@ -0,0 +1,9 @@
+/**
+ * Fitted Functions - Fitted Wardrobe Services
+ *
+  # Fitted Wardrobe Services
+
+ Creates the core fitting spec roquests with:
+ - Table preper calculations
+ - Wardrobe fitting specs
+ - Alternative immediate delivery fittings
